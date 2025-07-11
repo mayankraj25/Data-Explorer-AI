d9418c1d63166895ac31ed03d32f5a17e0854846
Mayank Raj <88675779+mayankraj25@users.noreply.github.com>
2025-07-12 01:42:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,20 @@ from langchain_openai import ChatOpenAI,OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 
-
-
-
 def load_csv_as_docs(df):
     documents = dataframe_to_document(df)
     return documents
 
-
-def build_agent(docs):
-    splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
-    chunks=splitter.split_documents(docs)
-    api_key = st.session_state.get("OPENAI_API_KEY")
+def build_agent(documents, api_key):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+    chunks = splitter.split_documents(documents)
     embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-    llm = ChatOpenAI(openai_api_key=api_key, model_name="gpt-4o-mini", temperature=0.2)
-    vectorstore=FAISS.from_documents(chunks,embeddings)
-    retriever=vectorstore.as_retriever()
+    vectorstore = FAISS.from_documents(chunks, embeddings)
 
-    return {"llm": llm, "retriever": retriever}
+    retriever = vectorstore.as_retriever()
+    llm = ChatOpenAI(openai_api_key=api_key, model_name="gpt-4o-mini", temperature=0.2)
+
+    return {
+        "llm": llm,
+        "retriever": retriever
+    }
